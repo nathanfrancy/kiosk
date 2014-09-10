1e514dae0b36898f81b25b10e724f2ed089d3bf5
Nathan Francy <nathanfrancy@gmail.com>
2014-09-10 18:33:01 -0500

    Nav buttons for getting around login system

--- home-admin.php
+++ home-admin.php
@@ -1,1 +1,2 @@
 Hello admin!
+<a href="logout.php">Logout</a>
--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 session_start();
 
+if ( isset($_SESSION['auth_editor_id']) || isset($_SESSION['auth_admin_id']) ) { 
+    echo "<a href='home.php'>Go home</a>"; 
+}
+else {
+    echo "<a href='login.php'>Login</a>";
+}
