589d887fd3bd27c549e7d3a5f9614cfa2a597d4c
Nathan Francy <nathanfrancy@gmail.com>
2014-09-26 11:52:34 -0500

    move login view to views/

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ if ( isset($_SESSION['auth_editor_id']) || isset($_SESSION['auth_admin_id']) ) {
     echo "<a href='home.php'>Go home</a>"; 
 }
 else {
-    echo "<a href='login.php'>Login</a>";
+    echo "<a href='views/login.php'>Login</a>";
 }
--- views/login.php
+++ views/login.php
@@ -4,7 +4,7 @@
  * Start the session and redirect if the user is logged in
  */
 session_start();
-if (isset($_SESSION['auth_id'])) { header("Location: home.php"); }
+if (isset($_SESSION['auth_id'])) { header("Location: ../home.php"); }
 
 /*=========================================================
  * If there is feedback, save in variable for later
@@ -21,9 +21,9 @@ if (isset($_GET['feedback'])) { $feedback = $_GET['feedback']; $feedbackValid = 
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/signin.css">
+	<link rel="stylesheet" href="../css/bootstrap.min.css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/signin.css">
 	<script src="js/jquery.js"></script>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sign In</title>
@@ -32,7 +32,7 @@ if (isset($_GET['feedback'])) { $feedback = $_GET['feedback']; $feedbackValid = 
 <body>
 
 	<div class="container">
-		<form class="form-signin" action="controllers/controller_login.php" method="post" role="form">
+		<form class="form-signin" action="../controllers/controller_login.php" method="post" role="form">
 			<h1>Login</h1>
 <!--<img src="img/logo.png" class="img-responsive"> -->
 			<input type="hidden" name="controllerType" value="userLogin">
