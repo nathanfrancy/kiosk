07b21647170149378553b9c2530028d5d3968329
Nathan Francy <nathanfrancy@gmail.com>
2014-10-29 17:54:18 -0500

    changes to front end

--- assets/js/public.js
+++ assets/js/public.js
@@ -8,7 +8,7 @@ function refreshDepartments() {
 	$.get( "api/?requestType=getDepartments", function(data) {
 		departments = jQuery.parseJSON(data);
 		for (var i = 0; i < departments.departments.length; i++) {
-			$("#list-group-departments").append('<a href="#" class="list-group-item list-group-item-department" departmentid="'+ departments.departments[i].id +'">'+ departments.departments[i].name +'</a>');
+			$("#list-group-departments").append('<a href="#" class="list-group-item list-group-item-department" departmentid="'+ departments.departments[i].id +'"><h4 class="list-group-item-heading">'+ departments.departments[i].name +'</h4></a>');
 		}
 	});
 }
@@ -35,16 +35,47 @@ $(document).on("click", ".list-group-item-newspost", function(e) {
 $(document).on("click", ".list-group-item-department", function(e) {
 	$("#list-group-professors").html('');
 	var id = parseInt($(this).attr("departmentid"));
+    var name = $(this).find(".list-group-item-heading").html();
 	$.get( "api/?requestType=getDepartmentProfessors&id=" + id, function(data) {
 		professors = jQuery.parseJSON(data);
-		for (var i = 0; i < professors.professors.length; i++) {
-			$("#list-group-professors").append('<a href="#" class="list-group-item list-group-item-professor" professorid="' + professors.professors[i].id + '"><h4 class="list-group-item-heading">'+ professors.professors[i].lastname + ', '+ professors.professors[i].firstname +'</h4><p class="list-group-item-text">' + professors.professors[i].officebuilding + ' ' + professors.professors[i].officeroom + '</p></a>');
-		}
+		showProfessors(professors);
+        $("#selected-section").html(name);
 	});
-}); 
+});
+
+function showProfessors() {
+    if (professors.professors.length !== 0) {
+        for (var i = 0; i < professors.professors.length; i++) {
+            $("#list-group-professors").append('<a href="#" class="list-group-item list-group-item-professor" professorid="' + professors.professors[i].id + '"><h4 class="list-group-item-heading">'+ professors.professors[i].lastname + ', '+ professors.professors[i].firstname +'</h4><p class="list-group-item-text">' + professors.professors[i].officebuilding + ' ' + professors.professors[i].officeroom + '</p></a>');
+        }
+    }
+    else {
+        $("#list-group-professors").html("No professors found.");
+    }
+    $("#pub-container-department-input").fadeOut("fast");
+
+    setTimeout(function() {
+        $("#pub-container-professor-input").fadeIn();
+    }, 350);
+
+}
+
+$(document).on("click", "#filter-lastname-container button", function(e) {
+	$("#list-group-professors").html('');
+	var letter = $(this).html();
+    name = 'Last names starting with "' + letter + '"';
+	$.get( "api/?requestType=getProfessorsWithLastName&letter="+ letter, function(data) {
+		professors = jQuery.parseJSON(data);
+		showProfessors(professors);
+        $("#selected-section").html(name);
+	});
+});
+
 
 $(document).on("click", ".list-group-item-professor", function(e) {
 	var id = parseInt($(this).attr("professorid"));
+    $("#courses").html('');
+    $("#officehours").html('');
 	$.get( "api/?requestType=getProfessor&id=" + id, function(data) {
 		professor = jQuery.parseJSON(data);
 		$("#professorModal").modal('show');
@@ -69,12 +100,57 @@ refreshNewsPosts();
 
 $("#filter-Program").click(function(e){
 	e.preventDefault();
-	$("#list-group-departments").show();
-	$("#filter-lastname-container").hide();
+	$("#filter-lastname-container").fadeOut("fast");
+    setTimeout(function() {
+        $("#list-group-departments").fadeIn();
+    }, 350);
+    $("#filter-lastName, #filter-Program").removeClass("active");
+    $(this).addClass("active");
 });
 
 $("#filter-lastName").click(function(e){
 	e.preventDefault();
-	$("#list-group-departments").hide();
-	$("#filter-lastname-container").show();
+	$("#list-group-departments").fadeOut("fast");
+    setTimeout(function() {
+        $("#filter-lastname-container").fadeIn();
+    }, 350);
+    $("#filter-lastName, #filter-Program").removeClass("active");
+    $(this).addClass("active");
 });
+
+
+$("#nav-button-professor").click(function() {
+    $("#nav-button-professor, #nav-button-news").removeClass("active");
+    $(".pub-news-master").fadeOut("fast");
+    setTimeout(function() {
+        $(".pub-department-master").fadeIn();
+    }, 350);
+    $(this).addClass("active");
+});
+
+$("#nav-button-news").click(function() {
+    $("#nav-button-professor, #nav-button-news").removeClass("active");
+    $(".pub-news-master").show();
+    $(".pub-department-master").hide();
+    $(this).addClass("active");
+});
+
+$("#nav-back-button").click(function() {
+    $("#pub-container-professor-input").fadeOut("fast");
+    setTimeout(function() {
+        $("#pub-container-department-input").fadeIn();
+    }, 350);
+});
+
+var timeoutHandle = window.setInterval(function() {
+    window.location.href = "index.php";
+}, 60000);
+
+$(document).click(function(e) {
+    window.clearInterval(timeoutHandle);
+    timeoutHandle = window.setInterval(function() {
+        window.location.href = "index.php";
+    }, 60000);
+});
+
+$(".pub-news-master").hide();
--- index.php
+++ index.php
@@ -32,37 +32,105 @@ if ( isset($_SESSION['auth_id']) ) {
 		  <div id="alertBoxBody" class="alert" role="alert"></div>
 	    </div>
 		
-		<div class="pub-header-box">
-			kiosk
-		</div>
+		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="border: 0px white solid;">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">kiosk</a>
+            </div>
+              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active" id="nav-button-professor"><a href="#">Find a Professor</a></li>
+                <li id="nav-button-news"><a href="#">News Posts</a></li>
+              </ul>
+              <ul class="nav navbar-nav navbar-right">
+                <!--<li><a href="#">Link</a></li>-->
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Help <span class="caret"></span></a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li><a href="#">Help Section</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Help Section 2</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div>
+        </nav>
     
+
         <div class="pub-container-master group">
-			<div class="pub-department-master">
-				<div class="">
-					<h2>Find a Professor</h2>
-					<div class="btn-group">
-					  <button type="button" id="filter-Program" class="btn btn-default">By Program</button>
-					  <button type="button" id="filter-lastName" class="btn btn-default">By Last Name</button>
-					</div>
-					<br>
-					<div class="row" style="margin-top: 10px;">
-						<div class="col-sm-4">
+			<div class="container-fluid pub-department-master">
+					<h1 class="text-center">Find a Professor</h1>
+
+                        <div id="pub-container-department-input" class="col-sm-4 col-sm-offset-4">
+
+                            <div class="text-center">
+                                <div class="btn-group btn-group-lg" style="margin-bottom: 10px; margin-top: 10px;">
+                                  <button type="button" id="filter-Program" class="btn btn-default active">By Program</button>
+                                  <button type="button" id="filter-lastName" class="btn btn-default">By Last Name</button>
+                                </div>
+                            </div>
+
+                            <br>
 							<div class="list-group" id="list-group-departments"></div>
+
 							<div id="filter-lastname-container">
-								<button type="button" class="btn btn-primary btn-sm">A</button>
+                                <center>
+                                    <br>
+                                    <button type="button" class="btn btn-primary btn-lg">A</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">B</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">C</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">D</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">E</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">F</button>
+                                    <br><br>
+                                    <button type="button" class="btn btn-primary btn-lg">G</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">H</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">I</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">J</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">K</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">L</button>
+                                    <br><br>
+                                    <button type="button" class="btn btn-primary btn-lg">M</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">N</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">O</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">P</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">Q</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">R</button>
+                                    <br><br>
+                                    <button type="button" class="btn btn-primary btn-lg">S</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">T</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">U</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">V</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">W</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">X</button>
+                                    <br><br>
+                                    <button type="button" class="btn btn-primary btn-lg">Y</button>&nbsp;
+                                    <button type="button" class="btn btn-primary btn-lg">Z</button>
+                                </center>
 							</div>
 						</div>
-						<div class="col-sm-8">
-							<div class="list-group" id="list-group-professors"></div>
-						</div>
-					</div>
-				</div>
+                        <div id="pub-container-professor-input">
+                            <div class="col-sm-3"></div>
+                            <div class="col-sm-6">
+                                <h2 id="selected-section" class="text-center"></h2>
+                                <button class="btn btn-default text-center" id="nav-back-button"><span class="glyphicon glyphicon-arrow-left"></span> &nbsp;Back</button>
+                                <div class="list-group" id="list-group-professors"></div>
+                            </div>
+                            <div class="col-sm-3"></div>
+                        </div>
+
 			</div>
 			
-			<div class="pub-news-master">
-				<h2>Recent Posts</h2>
-					<div class="list-group" id="list-group-newspost"></div>
-
+			<div class="container-fluid pub-news-master">
+				<h1 class="text-center" style="margin-bottom: 10px;">Recent Posts</h1>
+				<div class="list-group col-sm-6 col-sm-offset-3" id="list-group-newspost"></div>
 			</div>
 		</div>
 		
